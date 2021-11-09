53fcd51f89b54511de92e16ec419ce069101b71d
blueheart <blueheart@tuta.io>
2021-11-09 16:24:17 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from typing import Optional
+from fastapi import FastAPI
+from fastapi.param_functions import Body
+from pydantic import BaseModel
+from random import randrange
+
+app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
+            {"title": "favorite foods", "content": "I like pizza", "id": 2}]
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World!!"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": my_posts}
+
+
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 10000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+
+@app.get("/posts/{id}")
+def get_post(id):
+    print(id)
+    return {"post_detail": f"Here is post {id}"}
